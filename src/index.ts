0d1dcfa78d03fb9e0507d70712001c2160c01ecd
tangql <360603837@qq.com>
2022-06-29 17:35:47 +0800

    update: css build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import type { App, Plugin } from 'vue';
 import AsyncWrapper from './components/AsyncWrapper.vue';
+import './index.less';
 AsyncWrapper.install = (app: App): App => {
   app.component(AsyncWrapper.name, AsyncWrapper);
   return app;
